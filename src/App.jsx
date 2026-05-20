784d4c547b851b3c6527713c7d7cbd979f5a8997
Gabriel42 <gabriel8822@gmail.com>
2026-05-20 20:12:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -114,6 +114,10 @@ function Modal({ c, onClose, catColor }) {
             <i className="ti ti-mail" style={{ verticalAlign: "-2px", marginRight: 6 }} aria-hidden="true" />
             Contacter {c.name.split(" ")[0]}
           </a>
+          <a href={`mailto:${c.email}`} style={{ display: "block", textAlign: "center", padding: "10px 0", borderRadius: 10, background: catColor, color: "#fff", fontWeight: 500, fontSize: 14, textDecoration: "none", marginTop: 8 }}>
+            <i className="ti ti-mail" style={{ verticalAlign: "-2px", marginRight: 6 }} aria-hidden="true" />
+            voic CV {c.name.split(" ")[0]}
+          </a>
         </div>
       </div>
     </div>
